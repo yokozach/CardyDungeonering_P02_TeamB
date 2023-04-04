9eab906553c55ddb94b4c5ead326fd7fc92cacae
Yoko <yokozach2000@gmail.com>
2023-04-04 12:36:03 -0500

    Creation Of Scriptable Object for Tiles

---
+++ Assets/Scriptable Objects/Tile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tile", menuName = "Tile")]
+public class Tile : ScriptableObject
+{
+
+    public Sprite Image;
+
+    public GameObject Event;
+
+}
