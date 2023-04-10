e16dbf7b4fa5a8f070bc79869be8b7a4938dc0c4
Yoko <yokozach2000@gmail.com>
2023-04-10 13:21:33 -0500

    Test Tile Template Made
    created a test template of the Tiles

---
+++ Assets/Zachary Assets/Zach_Scripts/EventTile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTile : MonoBehaviour
+{
+
+    [SerializeField] public GameObject EventObject;
+   public void PlayEvent()
+    {
+        EventObject.SetActive(true);
+        
+    }
+}
